1c75a299c3b953a0baa785dc5b0cbba003491e97
Thiago Bomfim <thiago.bomfim@smartgreen.net>
2021-03-19 15:11:50 -0300

    default dict and setdefault

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 student_grades = {
     'Jack': [85, 90],
     'Jill': [80, 95]
@@ -23,6 +25,7 @@ def get_grades_with_assignment(name):
 def get_grades_with_assignment_better(name):
     return student_grades.setdefault(name, [])
 
+# the naive way
 def set_grade_naive(name, score):
     if name in student_grades:
         grades = student_grades[name]
@@ -30,3 +33,31 @@ def set_grade_naive(name, score):
         student_grades[name] = []
         grades = student_grades[name]
     grades.append(score)
+
+# the better way
+def set_grade_better(name, score):
+    grades = get_grades_with_assignment_better(name)
+    grades.append(score)
+
+# defaultdict is a dictionary with a default value
+# everytime we try to acess a key that doesn't exist
+# it will bind the default value to that key
+# student_grades = defaultdict(list)
+
+# we can use define the default value like this
+# student_grades = defaultdict(lambda:'default value')
+
+# even better way
+def set_grade_best(name, score):
+    #Here we add a score to the key called name
+    #if that key doesn't exist then the defaultdict
+    #applies the an empty list to the key
+    
+    
+# The second argument of defaultdict is the initial dict
+# which means that it is the dict from where you want to create
+# the default dict
+student_grades = defaultdict(list, {
+    'Jack': [85, 90],
+    'Jill': [80, 95]
+})
